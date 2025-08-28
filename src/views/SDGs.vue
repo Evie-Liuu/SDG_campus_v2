95d490237d4bac57212e31d54e6bf84a318aee06
Evie <cone306915@gmail.com>
2025-08-28 13:41:14 +0800

    排版

--- src/views/SDGs.vue
+++ src/views/SDGs.vue
@@ -13,28 +13,26 @@
       <div class="w-1/3"></div>
     </div>
   </header>
-  <main class="">
-    <section
-      class="grid grid-cols-1 md:grid-cols-4 gap-5 p-4 md:p-20 pt-10 text-center text-3xl md:min-h-230"
+  <main
+    class="grid grid-cols-1 md:grid-cols-4 gap-5 p-4 md:p-20 pt-10 text-center text-3xl md:min-h-220"
+  >
+    <div
+      class="col-span-1 md:col-span-3 h-48 md:h-auto border p-4 bg-[url(@/assets/images/data-bg.jpg)] bg-cover bg-top overflow-hidden flex items-center justify-center animate-fade-in-left animate-delay-100"
     >
-      <div
-        class="col-span-1 md:col-span-3 h-48 md:h-auto border p-4 bg-[url(@/assets/images/data-bg.jpg)] bg-cover bg-top overflow-hidden flex items-center justify-center animate-fade-in-left animate-delay-100"
-      >
-        <div class="absolute inset-0 bg-black/40"></div>
-        數據呈現
-      </div>
-      <div
-        class="col-span-1 h-48 md:h-auto border p-4 flex items-center justify-center animate-fade-in-left animate-delay-200"
-      >
-        <div class="absolute inset-0 bg-black/40"></div>
-        下載資源
-      </div>
-      <router-link
-        to="/story"
-        class="col-span-1 md:col-span-4 h-48 md:h-auto border p-4 bg-[url(@/assets/images/CS_school.png)] bg-cover bg-center overflow-hidden flex items-center justify-center animate-fade-in-left animate-delay-300 hover:shadow-[10px_5px_10px_-3px_rgba(0,0,0,0.8)] hover:font-bold"
-        >故事牆</router-link
-      >
-    </section>
+      <div class="absolute inset-0 bg-black/40"></div>
+      數據呈現
+    </div>
+    <div
+      class="col-span-1 h-48 md:h-auto border p-4 flex items-center justify-center animate-fade-in-left animate-delay-200"
+    >
+      <div class="absolute inset-0 bg-black/40"></div>
+      下載資源
+    </div>
+    <router-link
+      to="/story"
+      class="col-span-1 md:col-span-4 h-48 md:h-auto border p-4 bg-[url(@/assets/images/CS_school.png)] bg-cover bg-center overflow-hidden flex items-center justify-center animate-fade-in-left animate-delay-300 hover:shadow-[10px_5px_10px_-3px_rgba(0,0,0,0.8)] hover:font-bold"
+      >故事牆</router-link
+    >
   </main>
 </template>
 <script setup>
