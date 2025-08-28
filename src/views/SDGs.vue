8fbd2fda3accbbd89b7299d4713afcf0ca0a8b07
Evie <cone306915@gmail.com>
2025-08-28 12:35:39 +0800

    排版

--- src/views/SDGs.vue
+++ src/views/SDGs.vue
@@ -18,13 +18,13 @@
       class="grid grid-cols-1 md:grid-cols-4 gap-5 p-4 md:p-20 pt-10 text-center text-3xl md:min-h-230"
     >
       <div
-        class="col-span-1 md:col-span-3 h-48 md:h-auto border p-4 bg-[url(@/assets/images/data-bg.jpg)] bg-cover bg-top overflow-hidden flex items-center justify-center animate-fade-in-left animate-delay-100 hover:shadow-[10px_5px_10px_-3px_rgba(0,0,0,0.8)] hover:font-bold"
+        class="col-span-1 md:col-span-3 h-48 md:h-auto border p-4 bg-[url(@/assets/images/data-bg.jpg)] bg-cover bg-top overflow-hidden flex items-center justify-center animate-fade-in-left animate-delay-100"
       >
         <div class="absolute inset-0 bg-black/40"></div>
         數據呈現
       </div>
       <div
-        class="col-span-1 h-48 md:h-auto border p-4 flex items-center justify-center animate-fade-in-left animate-delay-200 hover:shadow-[10px_5px_10px_-3px_rgba(0,0,0,0.8)] hover:font-bold"
+        class="col-span-1 h-48 md:h-auto border p-4 flex items-center justify-center animate-fade-in-left animate-delay-200"
       >
         <div class="absolute inset-0 bg-black/40"></div>
         下載資源
